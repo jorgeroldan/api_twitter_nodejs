2d1e35c877669f789a341f434f10e445f2ef94af
Jorge Roldán <roldan21@gmail.com>
2018-11-05 21:56:28 -0300

    order app and server

---
+++ app/js/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    consumer_key: '1U2FGxwZLhsa1Kjk3FApycJPt',
+    consumer_secret: 'H1uajdZw9ndOnNTODcfD6moXOaRtqyPCI4Y8sOU7zmaJxGCpBV',
+    access_token: '37817078-na64qW3qReBuEYhBEgXETqA2oPDqqnYT4QAB5pGm9',
+    access_token_secret: '5ffjngCE2yX5mPVSd50HB4tkJGkrkMKZBsFd6HfTOpmAF'
+}
+
